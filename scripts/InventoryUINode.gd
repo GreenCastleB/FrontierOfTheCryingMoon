75fe4e9b22eb65169553f78bc42a477fd3064bb7
GreenCastleB <secondepoch@gmail.com>
2026-02-17 16:14:45 -0600

    MainNode State Machine
    Also added devilchildren font and day music

--- scripts/InventoryUINode.gd
+++ scripts/InventoryUINode.gd
@@ -2,10 +2,12 @@ extends Control
 
 # The Inventory UI Node
 
+const FULL_SIZE:int = 102;
+
 ## 0.0 is hidden, 1.0 is shown.
 @export_range(0.0,1.0) var progress:float = 1.0:
 	set(newVal):
 		progress = newVal;
-		#%MainContainer.visible = (progress == 1.0);
-		%Background.set_deferred("custom_minimum_size", Vector2(roundi(102 * progress), 0));
-		self.set_deferred("custom_minimum_size", Vector2(roundi(102 * progress), 0));
+		var custMinSize:int = roundi(FULL_SIZE * progress);
+		%Background.set_deferred("custom_minimum_size", Vector2(custMinSize, 0));
+		self.set_deferred("custom_minimum_size", Vector2(custMinSize, 0));
--- scripts/MainNode.gd
+++ scripts/MainNode.gd
@@ -2,16 +2,48 @@ extends MarginContainer
 
 # The Main Node
 
+## State machine for Main Node.
+enum STATE {INIT, WALKING, ENTER_DIALOG, DIALOG, EXIT_DIALOG};
+const STATESTR:Array = ['INIT', 'WALKING', 'ENTER_DIALOG', 'DIALOG', 'EXIT_DIALOG'];
+var currState:STATE = STATE.INIT:
+	set(newState):
+		var oldState:STATE = currState;
+		currState = newState;
+		var stateChange:String = STATESTR[oldState] + "->" + STATESTR[newState];
+		printt("MainNode ::", "stateChange", stateChange);
+		match stateChange:
+			"WALKING->ENTER_DIALOG":
+				%DialogUI.show();
+				%Anim.play("ShowDialog");
+			"ENTER_DIALOG->DIALOG":
+				pass;
+			"DIALOG->EXIT_DIALOG":
+				%InventoryUI.show();
+				%Anim.play("HideDialog");
+			"EXIT_DIALOG->WALKING":
+				pass;
+
+func _ready() -> void:
+	printt("MainNode ::", "_ready");
+	currState = STATE.WALKING;
+
 func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("ui_right")):
-		printt("MainNode ::", "SHOW DIALOG");
-		%DialogUI.show();
-		%Anim.play("ShowDialog");
-		await %Anim.animation_finished;
-		%InventoryUI.hide();
-	if (event.is_action_pressed("ui_left")):
-		printt("MainNode ::", "HIDE DIALOG");
-		%InventoryUI.show();
-		%Anim.play("HideDialog");
-		await %Anim.animation_finished;
-		%DialogUI.hide();
+	if currState in [STATE.WALKING]:
+		if (event.is_action_pressed("ui_right")):
+			printt("MainNode ::", "Walking Input", "ui_right");
+			currState = STATE.ENTER_DIALOG;
+	elif currState in [STATE.DIALOG]:
+		if (event.is_action_pressed("ui_left")):
+			printt("MainNode ::", "Dialog Input", "ui_left");
+			currState = STATE.EXIT_DIALOG;
+
+func _on_anim_finished(anim_name: StringName) -> void:
+	var animFin:String = STATESTR[currState] + ":" + anim_name;
+	printt("MainNode ::", "animFin", animFin);
+	match animFin:
+		"ENTER_DIALOG:ShowDialog":
+			%InventoryUI.hide();
+			currState = STATE.DIALOG;
+		"EXIT_DIALOG:HideDialog":
+			%DialogUI.hide();
+			currState = STATE.WALKING;
